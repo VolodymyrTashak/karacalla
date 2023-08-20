187c10a5a3b9da565fd3a7eafd3309dd85d604db
VolodymyrTashak <vovavozny33@gmail.com>
2023-08-20 20:01:03 +0300

    fin btn-top

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,13 @@
+import { ScrollToTop } from "components/BtnTop/BtnTop";
+
 import { Head, HeaderContainer, HeaderLogo, HeaderBox, HeaderList, HeaderItem, HeaderItemLink } from "./Header.styled";
 
 export const Header = () => {
 
-  (function () {
-  
-    function trackScroll() {
-      var scrolled = window.pageYOffset;
-      var coords = document.documentElement.clientHeight;
-  
-      if (scrolled > coords) {
-        goTopBtn.classList.add('back-to-top-show');
-      }
-      if (scrolled < coords) {
-        goTopBtn.classList.remove('back-to-top-show');
-      }
-    }
-  
-    function backToTop() {
-      window.scrollTo(0, 0);
-    }
-  
-    var goTopBtn = document.querySelector('.back-to-top');
-  
-    window.addEventListener('scroll', trackScroll);
-    goTopBtn.addEventListener('click', backToTop);
-  })();
-
     return (
       <Head>
             <HeaderContainer className="container">
-              <button className="back-to-top"></button>
+              <ScrollToTop />
              <HeaderLogo href="#">karacalla</HeaderLogo>
              <HeaderBox>
               <HeaderList>
